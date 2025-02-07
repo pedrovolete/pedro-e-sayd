4e19bb74f0776f2a26c4b71f7980d4e849545b7d
pedrovolete <pedro.volete.silva@escola.pr.gov.br>
2025-02-07 08:56:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = prompt('Escreva seu nome:');
+let idade = prompt('Digite sua idade:');
+
+let ano = 2025 - idade;
+
+alert(`Seja bem-vindo ${nome}, você nasceu em ${ano}`);
+console.log(`Seja bem-vindo ${nome}, você nasceu em ${ano}`);
+document.write(`Seja bem-vindo ${nome}, você nasceu em ${ano}`);
